cbc3d3bfa3dc198ada206e9a6c5218626a9c7380
Shane <shanemcdermott@unomaha.edu>
2018-02-28 11:04:42 -0600

    Added CityGen Editor Window boilerplate.

--- MechanicalTurk/Assets/Editor/CityGenEditorWindow.cs
+++ MechanicalTurk/Assets/Editor/CityGenEditorWindow.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class CityGenEditorWindow : MonoBehaviour {
+public class CityGenEditorWindow : EditorWindow
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [MenuItem("Window/Feature Comparison")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CityGenEditorWindow));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Coming soon!");
+    }
+
 }
