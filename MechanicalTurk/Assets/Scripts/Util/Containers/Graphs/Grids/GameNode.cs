c4a2cb3406525b97f483ec9651e5635488c2201d
Shane <shanemcdermott@unomaha.edu>
2018-02-28 17:01:51 -0600

    Added District based building placement

--- MechanicalTurk/Assets/Scripts/Util/Containers/Graphs/Grids/GameNode.cs
+++ MechanicalTurk/Assets/Scripts/Util/Containers/Graphs/Grids/GameNode.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameNode : MonoBehaviour
 {
+    //Level of detail prefabs. Each should have a GameNode Component;
+    public GameObject[] prefabOptions;
+    public Vector3 spawnSpace = new Vector3(25, 0, 25);
+    public Vector3 minLocation = new Vector3(-50,0, 50);
+    public Vector3 maxLocation = new Vector3(-50,0, 50);
+
     protected Node node;
 
     public void SetNode(Node node)
@@ -17,6 +23,24 @@ public class GameNode : MonoBehaviour
 		
 	}
 	
+    public void SpawnBuildings()
+    {
+      for(float x = minLocation.x; x < maxLocation.x; x+=spawnSpace.x)
+        {
+            for(float z = minLocation.z; z < maxLocation.z; z+=spawnSpace.z)
+            {
+                GameObject go = GameObject.Instantiate(GetRandomPrefab());
+                go.transform.SetParent(transform);
+                go.transform.localPosition = new Vector3(x,0,z);
+            }
+        }
+    }
+
+    public GameObject GetRandomPrefab()
+    {
+        return prefabOptions[Random.Range(0, prefabOptions.Length)];
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
