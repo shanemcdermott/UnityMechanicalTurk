3d8845d5ae9ab6cef5e2c7ed5dbe5d993ac9a806
ambid17 <sbambrose@unomaha.edu>
2018-02-27 21:45:27 -0600

    added slope accounting

--- MechanicalTurk/Assets/Scripts/Generation/ClutterPlacement.cs
+++ MechanicalTurk/Assets/Scripts/Generation/ClutterPlacement.cs
@@ -36,20 +36,34 @@ public class ClutterPlacement : MonoBehaviour
 
     float GetHeightAtPostion(Vector2 v2)
     {
-        Mesh mapMesh = mapMeshFilter.mesh;
+        Mesh mapMesh = mapMeshFilter.sharedMesh;
         Vector3[] meshVerts = mapMesh.vertices;
-        Vector3[] meshNormals = mapMesh.normals;
+        
 
         for(int x = 0; x < meshVerts.Length; x++)
         {
-            //if(meshVerts[x].x - v2.x < 3 && meshVerts[x].z - v2.y < 3 && meshNormals[x].)
-            //{
-            //    Debug.Log("found height");
-            //    return meshVerts[x].y;
-            //}
+            if (meshVerts[x].x - v2.x < 3 && meshVerts[x].z - v2.y < 3 && CheckSlopeForPlacement(x))
+            {
+                return meshVerts[x].y;
+            }
         }
 
         return 0;
+    }
+
+    bool CheckSlopeForPlacement(int meshIndice)
+    {
+        Mesh mapMesh = mapMeshFilter.sharedMesh;
+        Vector3[] meshNormals = mapMesh.normals;
+        Vector3 normalToCheck = meshNormals[meshIndice];
+        float zySlope = normalToCheck.y / normalToCheck.z;
+        float xySlope = normalToCheck.y / normalToCheck.x;
+
+        if(Mathf.Abs(zySlope) < 0.2f && Mathf.Abs(xySlope) < 0.2f)
+        {
+            return true;
+        }
+        return false;
     }
 
     public void clearClutter()
