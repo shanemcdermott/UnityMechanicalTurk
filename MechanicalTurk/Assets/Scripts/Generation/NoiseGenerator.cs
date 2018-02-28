c7beb86ea431c8911fd60143df6f676924c27dbd
Shane <shanemcdermott@unomaha.edu>
2018-02-27 20:32:43 -0600

    Added Noise Generation interface, CityGeneration Editor Window.

---
+++ MechanicalTurk/Assets/Scripts/Generation/NoiseGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NoiseAlgorithm
+{
+    Simplex,
+    Perlin
+}
+
+public interface NoiseGenerator
+{
+   float[,] GenerateHeightmap(int width, int height);
+}
