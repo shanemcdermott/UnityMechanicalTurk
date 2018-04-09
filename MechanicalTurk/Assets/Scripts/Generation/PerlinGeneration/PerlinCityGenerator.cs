0bf94f7c337e3b75b207c1f27d80287b309c0931
ambid17 <sambrose@nebrwesleyan.edu>
2018-04-09 18:09:37 -0500

    code cleanup

--- MechanicalTurk/Assets/Scripts/Generation/PerlinGeneration/PerlinCityGenerator.cs
+++ MechanicalTurk/Assets/Scripts/Generation/PerlinGeneration/PerlinCityGenerator.cs
@@ -37,8 +37,6 @@ public class PerlinCityGenerator : CityGenerator {
         GenerateRoadGrid();
         //SpawnTestObjects();
         CreateRoadsFromGrid();
-        //PopulateRoadTexture();
-        //ApplyRoadToTerrain();
         //GenerateBuildings();
     }
 
@@ -106,122 +104,6 @@ public class PerlinCityGenerator : CityGenerator {
 
         roadPainter.DrawRoads(ref connectionPoints);
         roadPainter.ApplyAlphaBlend();
-    }
-
-    void PopulateRoadTexture()
-    {
-        Debug.Log("PerlinCityGenerator: Populating Road Texture");
-        roadTexture = new Texture2D(terrainGenerator.terrain.terrainData.heightmapWidth, terrainGenerator.terrain.terrainData.heightmapHeight);
-
-        List<Node> vertices = polyGrid.GetVertices();
-        List<GridFace> faces = polyGrid.GetFaces();
-        
-        //draw connections between verts
-        foreach(Node node in vertices)
-        {
-            //if the value is true, it is horizontal, and should be red, vertical = green
-            Dictionary<Vector2Int, bool> connectionPoints = node.GiveConnectionLines();
-            foreach (KeyValuePair<Vector2Int,bool> kvp in connectionPoints)
-            {
-                if(kvp.Value == true)
-                {
-                    roadTexture.SetPixel(kvp.Key.x, kvp.Key.y, Color.red);
-                }
-                else
-                {
-                    roadTexture.SetPixel(kvp.Key.x, kvp.Key.y, Color.blue);
-                }
-                
-            }
-        }
-
-        //draw connections between face verts
-        for(int i = 0; i < faces.Count;i++)
-        {
-            Dictionary<Vector2Int, bool> connectionPoints = new Dictionary<Vector2Int, bool>();
-            CombineWith(connectionPoints, faces[i].GetConnectionLine(faces[i].GetVertex(0).GetPosition(), faces[i].GetVertex(1).GetPosition()));
-            CombineWith(connectionPoints, faces[i].GetConnectionLine(faces[i].GetVertex(0).GetPosition(), faces[i].GetVertex(2).GetPosition()));
-            CombineWith(connectionPoints, faces[i].GetConnectionLine(faces[i].GetVertex(2).GetPosition(), faces[i].GetVertex(3).GetPosition()));
-            CombineWith(connectionPoints, faces[i].GetConnectionLine(faces[i].GetVertex(1).GetPosition(), faces[i].GetVertex(3).GetPosition()));
-
-            foreach (KeyValuePair<Vector2Int,bool> kvp in connectionPoints)
-            {
-                if(kvp.Value == true)
-                {
-                    roadTexture.SetPixel(kvp.Key.x, kvp.Key.y, Color.red);
-                }
-                else
-                {
-                    roadTexture.SetPixel(kvp.Key.x, kvp.Key.y, Color.blue);
-                }
-                
-            }
-        }
-
-        roadTexture.Apply();
-
-        Material planeMat = testPlane.GetComponent<Renderer>().sharedMaterial;
-        planeMat.mainTexture = roadTexture;
-    }
-
-    void CombineWith(Dictionary<Vector2Int, bool> dict, Dictionary<Vector2Int, bool> dictToAdd)
-    {
-        foreach (KeyValuePair<Vector2Int, bool> kvp in dictToAdd)
-        {
-            bool value;
-            if (!dict.TryGetValue(kvp.Key, out value))
-            {
-                dict.Add(kvp.Key, kvp.Value);
-            }
-        }
-    }
-
-    void ApplyRoadToTerrain()
-    {
-        Debug.Log("PerlinCityGenerator: Applying Road Texture to terrain");
-        TerrainData tData = terrainGenerator.terrain.terrainData;
-        SplatPrototype[] textures = new SplatPrototype[3];
-
-        textures[0] = tData.splatPrototypes[0];
-
-        textures[1] = new SplatPrototype();
-        textures[1].texture = roadHorizontal;
-		textures[1].tileSize = new Vector2Int(1,1);
-
-        textures[2] = new SplatPrototype();
-        textures[2].texture = roadVertical;
-        textures[2].tileSize = new Vector2Int(1, 1);
-
-        float[,,] alphamaps = new float[tData.alphamapWidth, tData.alphamapHeight, 3];
-        
-        for (int y = 0; y < tData.alphamapHeight; y++)
-        { 
-            for (int x = 0; x < tData.alphamapWidth; x++)
-            {
-                //check if the road is at this x/y (normalized), if so, alpha=1, else alpha=0
-                Color color = roadTexture.GetPixel(x / 2, y / 2);
-                
-                if(color.b == 1){
-                    alphamaps[y, x, 0] = 0;
-                    alphamaps[y, x, 1] = 1;
-                    alphamaps[y, x, 2] = 0;
-                }
-                else if(color.r == 1)
-                {
-                    alphamaps[y, x, 0] = 0;
-                    alphamaps[y, x, 1] = 0;
-                    alphamaps[y, x, 2] = 1;
-                }
-                else
-                {
-                    alphamaps[y, x, 0] = 1;
-                    alphamaps[y, x, 1] = 0;
-                    alphamaps[y, x, 2] = 0;
-                }
-            }
-        }
-        terrainGenerator.terrain.terrainData.splatPrototypes = textures;
-        terrainGenerator.terrain.terrainData.SetAlphamaps(0, 0, alphamaps);
     }
 
     void GenerateBuildings()
