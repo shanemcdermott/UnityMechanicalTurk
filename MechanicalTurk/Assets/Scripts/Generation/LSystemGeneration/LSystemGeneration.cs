a1c71f494cdb614af177eacd3e3bffa340a14dab
Sam <samuel.groenjes@gmail.com>
2018-04-09 14:08:05 -0500

    Using dictionary over polyGrid
    Need to work on adding connections correctly

--- MechanicalTurk/Assets/Scripts/Generation/LSystemGeneration/LSystemGeneration.cs
+++ MechanicalTurk/Assets/Scripts/Generation/LSystemGeneration/LSystemGeneration.cs
@@ -8,19 +8,21 @@ public class LSystemGeneration : CityGenerator
     public TerrainGenerator terrainGenerator;
     public Texture2D[] tilingTextures;
     private int seed;
-    private List<Vector3> roads = new List<Vector3>();
     private string axiom;
     private float angle;
     private string currString;
     private float length = 1f;
     private int iterations;
     private Stack<TransformInfo> transformStack = new Stack<TransformInfo>();
+    private Dictionary<Vector2Int, Vector2Int[]> roadGrid = new Dictionary<Vector2Int, Vector2Int[]>();
     private Dictionary<char, string> rules = new Dictionary<char, string>();
+    private Vector2Int prev, curr;
     private Texture2D roadTexture;
 
 
     public override void Generate()
     {
+        
         GenerateRoadGrid();
         PopulateRoadTexture();
         ApplyRoadToTerrain();
@@ -50,28 +52,43 @@ public class LSystemGeneration : CityGenerator
                 alphamaps[x, y, 0] = 1;
             }
         }
-
-        foreach (Node node in polyGrid.GetVertices())
+        List<Vector2Int> vertices = new List<Vector2Int>(roadGrid.Keys);
+        foreach (Vector2Int node in vertices)
         {
-            List<IConnection<Node>> connections = new List<IConnection<Node>>();
-            node.GetConnections(out connections);
-            int i = 0;
-            foreach(IConnection<Node> edge in connections)
+            Vector2Int[] connections = new Vector2Int[4];
+            if (roadGrid.TryGetValue(node, out connections))
             {
-                Node n = edge.GetToNode();
-                Vector2 diff = n.GetPositionXZ() - node.GetPositionXZ();
-                if (diff.x > 0)
-                    i += 4;
-                if (diff.x < 0)
-                    i += 1;
-                if (diff.y > 0)
-                    i += 8;
-                if (diff.y < 0)
-                    i += 2;
+                int i = 0;
+                foreach (Vector2Int edge in connections)
+                {
+                    if (edge == Vector2Int.zero)
+                    {
+                        Debug.Log("Connection node of node (" + node.x + ", "+node.y + ") is zero!");
+                        break;
+                    }
+                    Vector2Int diff = edge - node;
+                    if (diff.x > 0)
+                        i += 4;
+                    if (diff.x < 0)
+                        i += 1;
+                    if (diff.y > 0)
+                        i += 8;
+                    if (diff.y < 0)
+                        i += 2;
+                }
+                Debug.Log("This iterator should be between 1-15: " + i);
+                Debug.Log("Connection Data for Vertex: x:" + (int)node.x + "  y: " + (int)node.y+
+                    " 1st connection: x:"+(int)connections[0].x+" y: "+(int)connections[0].y+
+                    " 2nd connection: x:" + (int)connections[1].x + " y: " + (int)connections[1].y +
+                    " 3rd connection: x:" + (int)connections[2].x + " y: " + (int)connections[2].y +
+                    " 4th connection: x:" + (int)connections[3].x + " y: " + (int)connections[3].y);
+                alphamaps[(int)node.x, (int)node.y, i] = 1;
+                alphamaps[(int)node.x, (int)node.y, 0] = 0;
             }
-            Debug.Log("number of connections is " +node.NumConnections());
-            alphamaps[(int)node.GetPosition().x, (int)node.GetPosition().y, i] = 1;
-            alphamaps[(int)node.GetPosition().x, (int)node.GetPosition().y, 0] = 0;
+            else
+            {
+                Debug.Log("Unable to set alphamap");
+            }
         }
 
         
@@ -84,12 +101,11 @@ public class LSystemGeneration : CityGenerator
         roadTexture = new Texture2D(terrainGenerator.terrain.terrainData.heightmapWidth, terrainGenerator.terrain.terrainData.heightmapHeight);
 
         Color color = Color.black;
+        List<Vector2Int> vertices = new List<Vector2Int>(roadGrid.Keys);
 
-        List<Node> vertices = polyGrid.GetVertices();
-
-        foreach (Node node in vertices)
+        foreach (Vector2Int node in vertices)
         {
-            Vector2 v2 = node.GetPositionXZ();
+            Vector2Int v2 = node;
             roadTexture.SetPixel((int)v2.x, (int)v2.y, color);
         }
 
@@ -107,21 +123,24 @@ public class LSystemGeneration : CityGenerator
         angle = 90f;
         iterations = 12;
         axiom = "FX";
+        
+        roadGrid.Add(new Vector2Int((int)transform.position.x,(int)transform.position.z), new Vector2Int[4]);
 
-        polyGrid.AddVertex(new Node(transform.position));
+
         currString = axiom;
         for (int i = 0; i < iterations; i++)
         {
             GenerateIteration();
         }
-        //DrawLines();
+        DrawLines();
     }
 
     void DrawLines()
     {
-        for (int i = 0; i < roads.Count - 1; i++)
+        List<Vector2Int> vertices = new List<Vector2Int>(roadGrid.Keys);
+        for(int i=0; i<vertices.Count-1;i++)
         {
-            Debug.DrawLine(roads[i], roads[i + 1], Color.black, 10000f, false);
+            Debug.DrawLine(new Vector3(vertices[i].x, 4, vertices[i].y), new Vector3(vertices[i+1].x,4,vertices[i+1].y), Color.black, 10000f, false);
         }
     }
 
@@ -154,10 +173,48 @@ public class LSystemGeneration : CityGenerator
             if (currentCharacter == 'F' || currentCharacter == 'G')
             {
                 index++;
+                prev = new Vector2Int((int)transform.position.x, (int)transform.position.z);
                 transform.Translate(Vector3.forward * length);
-                Node n = new Node(transform.position);
-                polyGrid.AddConnection(polyGrid.NumVertexConnections(polyGrid.NumVertices() - 1),n);
-                polyGrid.AddVertex(n);
+                curr = new Vector2Int((int)transform.position.x, (int)transform.position.z);
+                if (!roadGrid.ContainsKey(curr))//dictionary does not contain new node
+                {
+                    roadGrid.Add(curr, new Vector2Int[4]);
+                }
+                //add connection
+                /*Connections [0] - North
+                                [1] - East
+                                [2] - South
+                                [3] - West*/
+                Vector2Int[] CurrConnections = new Vector2Int[4];
+                Vector2Int[] PrevConnections = new Vector2Int[4];
+                if (roadGrid.TryGetValue(curr, out CurrConnections) && roadGrid.TryGetValue(prev, out PrevConnections))
+                {
+                    Vector2Int diff = curr - prev;
+                    if (diff.x > 0)//node curr is east of node prev
+                    {
+                        CurrConnections[3] = prev;
+                        PrevConnections[1] = curr;
+                    }  
+                    if (diff.x < 0)//node curr is west of node prev
+                    {
+                        CurrConnections[1] = prev;
+                        PrevConnections[3] = curr;
+                    }
+                    if (diff.y > 0)//node curr is north of node prev
+                    {
+                        CurrConnections[2] = prev;
+                        PrevConnections[0] = curr;
+                    }
+                    if (diff.y < 0)//node curr is south of node prev
+                    {
+                        CurrConnections[0] = prev;
+                        PrevConnections[2] = curr;
+                    }
+                }
+                else
+                {
+                    Debug.Log("Unable to add connection");
+                }
             }
             else if (currentCharacter == 'f')//will create 'islands' of roads (vertex not added, still move)
             {
