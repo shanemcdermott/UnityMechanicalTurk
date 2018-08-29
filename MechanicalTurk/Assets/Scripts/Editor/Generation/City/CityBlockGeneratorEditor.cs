88ae8827d79956a5e4c1e5946930d284ff2b4e19
Shane <shanemcdermott@unomaha.edu>
2018-08-29 13:53:53 -0500

    Added Point Generator button

--- MechanicalTurk/Assets/Scripts/Editor/Generation/City/CityBlockGeneratorEditor.cs
+++ MechanicalTurk/Assets/Scripts/Editor/Generation/City/CityBlockGeneratorEditor.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 
 using Framework.Generation;
 using Algorithms.City;
+using Algorithms.Noise;
 
 [CustomEditor(typeof(CityBlockGenerator))]
 public class CityBlockGeneratorEditor : GeneratorEditor
@@ -22,9 +23,33 @@ public class CityBlockGeneratorEditor : GeneratorEditor
 
     }
 
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(_controller.pointGenerator == null)
+        {
+            if (GUILayout.Button("Add Point Generator"))
+            {
+                CreatePointGenerator();
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
     public override void ShowMissingGenRequirements()
     {
         EditorGUILayout.LabelField("Missing Requirements", errorStyle);
         EditorGUILayout.PropertyField(_prefabs);
+    }
+
+
+    protected PoissonDiskSampling CreatePointGenerator()
+    {
+        PoissonDiskSampling pointGenerator = _controller.gameObject.AddComponent<PoissonDiskSampling>();
+        _controller.pointGenerator = pointGenerator;
+        return pointGenerator;
     }
 }
